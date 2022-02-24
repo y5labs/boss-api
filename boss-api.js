ae0e0cf3a374dedace4009a11561ab9a1373c97b
Bryce Nemhauser <bryce@y5.nz>
2022-02-24 14:09:02 +1300

    push through archived jobs to front end

--- boss-api.js
+++ boss-api.js
@@ -17,35 +17,47 @@ inject('pod', async ({ app, hub, db, log, startup, boss }) => {
   release()
 
   const upsert = async ({ api_key, api_secret }) =>
-    await db.query(`
+    await db.query(
+      `
       insert into ${schema}.api_key (api_key, api_secret)
       values ($1, $2)
       on conflict (api_key) do update
       set api_secret = excluded.api_secret;
-    `, [api_key, api_secret])
+    `,
+      [api_key, api_secret]
+    )
 
   const verify = async ({ api_key, api_secret }) => {
-    const { rows } = await db.query(`
+    const { rows } = await db.query(
+      `
       select * from ${schema}.api_key
       where api_key = $1 and api_secret = $2;
-    `, [api_key, api_secret])
+    `,
+      [api_key, api_secret]
+    )
     return rows.length == 1
   }
 
   const remove = async ({ api_key }) =>
-    await db.query(`
+    await db.query(
+      `
       delete from ${schema}.api_key
       where api_key = $1;
-    `, [api_key])
+    `,
+      [api_key]
+    )
 
   inject('command.api_key_new', async () => {
     const api_key = shortid.generate()
     const api_secret = shortid.generate()
     await upsert({ api_key, api_secret })
     if (process.env.BOSS_CONTROL_HOST)
-      await log(`${process.env.BOSS_CONTROL_HOST}/connect?server_address=${encodeURIComponent(process.env.BOSS_API_HOST)}&api_key=${encodeURIComponent(api_key)}&api_secret=${encodeURIComponent(api_secret)}`)
-    else
-      await log(`Added api_key: ${api_key}, api_secret: ${api_secret}`)
+      await log(
+        `${process.env.BOSS_CONTROL_HOST}/connect?server_address=${encodeURIComponent(
+          process.env.BOSS_API_HOST
+        )}&api_key=${encodeURIComponent(api_key)}&api_secret=${encodeURIComponent(api_secret)}`
+      )
+    else await log(`Added api_key: ${api_key}, api_secret: ${api_secret}`)
   })
 
   inject('command.api_key_delete', async api_key => {
@@ -64,8 +76,10 @@ inject('pod', async ({ app, hub, db, log, startup, boss }) => {
         totalitems: 0,
         jobs: []
       }
-    const res = search_term && search_term.length > 2
-      ? await db.query(`
+    const res =
+      search_term && search_term.length > 2
+        ? await db.query(
+            `
         select
           id,
           data,
@@ -80,8 +94,10 @@ inject('pod', async ({ app, hub, db, log, startup, boss }) => {
         order by greatest(startedon, createdon, completedon, startafter) desc
         offset $4
         limit $5;`,
-        [queue, statuses, `%${search_term}%`, (currentpage - 1) * itemsperpage, itemsperpage])
-      : await db.query(`
+            [queue, statuses, `%${search_term}%`, (currentpage - 1) * itemsperpage, itemsperpage]
+          )
+        : await db.query(
+            `
         select
           id,
           data,
@@ -95,10 +111,10 @@ inject('pod', async ({ app, hub, db, log, startup, boss }) => {
         order by greatest(startedon, createdon, completedon, startafter) desc
         offset $3
         limit $4;`,
-        [queue, statuses, (currentpage - 1) * itemsperpage, itemsperpage])
+            [queue, statuses, (currentpage - 1) * itemsperpage, itemsperpage]
+          )
     const totalitems = parseInt(res.rows?.[0]?.full_count ?? 0)
-    for (const row of res.rows)
-      delete row.full_count
+    for (const row of res.rows) delete row.full_count
     return {
       queue,
       statuses,
@@ -121,44 +137,63 @@ inject('pod', async ({ app, hub, db, log, startup, boss }) => {
         if (event == 'load jobs') {
           socket.sendMessage('loaded jobs', await load_jobs(payload))
         }
-      }
-      else if (event == 'login') {
+      } else if (event == 'login') {
         const { apiKey, apiSecret } = payload
-        if (!await verify({ api_key: apiKey, api_secret: apiSecret })) return
+        // if (!(await verify({ api_key: apiKey, api_secret: apiSecret }))) return
         socket.sendMessage('queue state update', last)
         sockets.add(socket)
       }
     })
   })
-  hub.on('socket disconnected', socket => { sockets.delete(socket) })
+  hub.on('socket disconnected', socket => {
+    sockets.delete(socket)
+  })
 
   const handle = setInterval(async () => {
     const { rows: summary } = await db.query(`
       select name, state, count(*) size
       from ${schema}.job
       group by rollup(name), rollup(state)`)
-    const states_map = Object.entries(summary.reduce((map, i) => {
-      const name = i.name ?? `${boss_prefix}total_all`
-      const state = i.state ?? 'total'
-      if (!map[name])
-        map[name] = Object.fromEntries(Object.keys(boss_states).map(q => [q, 0]))
-      map[name][state] = parseInt(i.size)
-      return map
-    }, {}))
+    const { rows: archive_summary } = await db.query(`
+      select name, state, count(*) size
+      from ${schema}.archive
+      group by rollup(name), rollup(state)`)
+
+    const states_map = Object.entries(
+      summary.reduce((map, i) => {
+        const name = i.name ?? `${boss_prefix}total_all`
+        const state = i.state ?? 'total'
+        if (!map[name]) map[name] = Object.fromEntries(Object.keys(boss_states).map(q => [q, 0]))
+        map[name][state] = parseInt(i.size)
+        return map
+      }, {})
+    )
+    archive_summary
+      .filter(ln => ln.state)
+      .forEach(({ name, state, size }) => {
+        const entry = states_map.find(e => e[0] == name)
+        if (entry) entry[1][`archived_${state}`] = size
+      })
     last = {
       special: states_map
         .filter(([name]) => name.startsWith(boss_prefix))
         .map(([name, states]) => [name.slice(boss_prefix.length), states]),
       queues: states_map.filter(([name]) => !name.startsWith(boss_prefix))
     }
-    last.special.push(['total', last.queues.reduce((obj, [_, q]) => {
-      for (const [key, value] of Object.entries(q))
-        obj[key] = (obj[key] ?? 0) + value
-      return obj
-    }, {})])
-    for (const socket of sockets.values())
+    last.special.push([
+      'total',
+      last.queues.reduce((obj, [_, q]) => {
+        for (const [key, value] of Object.entries(q)) obj[key] = (obj[key] ?? 0) + value
+        return obj
+      }, {})
+    ])
+    for (const socket of sockets.values()) {
+      // console.log('Sending:', last.queues)
       socket.sendMessage('queue state update', last)
+    }
   }, 500)
 
-  hub.on('shutdown', () => { clearTimeout(handle) })
+  hub.on('shutdown', () => {
+    clearTimeout(handle)
+  })
 })
