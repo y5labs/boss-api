21dd4acf4eaf160165cfb2455b957c1c4dc3d4fb
Bryce Nemhauser <bryce@y5.nz>
2022-04-07 17:15:20 +1200

    correct connection string usage

--- maintenance/migrate.js
+++ maintenance/migrate.js
@@ -6,18 +6,18 @@ const bossConnectionString2 = process.argv[3]
 const jobName = process.argv[4]
 
 const pool1 = new Pool({
-  bossConnectionString1
+  connectionString: bossConnectionString1
 })
 const pool2 = new Pool({
-  bossConnectionString2
+  connectionString: bossConnectionString2
 })
 console.log(bossConnectionString1)
 console.log(bossConnectionString2)
-const result = await pool1.query(`SELECT * FROM job WHERE name = ${jobName}`)
-console.log('result :>> ', result)
+const result = await pool1.query(`select * from job where name = '${jobName}'`)
+console.log('result :>> ', result.rows)
 
 // Execution:
 // e.g.
-// npm run migrate postgresql://whites_postgres_boss:HwyJWIPCFbG96iyDzhhMC99eWkkadJ5Z@localhost:5433/boss \
-// postgresql://whites_postgres_boss:HwyJWIPCFbG96iyDzhhMC99eWkkadJ5Z@localhost:8091/boss \
+// npm run migrate postgresql://whites_postgres:pPEaR94Ygjki8FXheKcm4AW0jfUo5tk1@localhost:5433/boss \
+// postgresql://whites_postgres_boss:HwyJWIPCFbG96iyDzhhMC99eWkkadJ5Z@localhost:5434/boss \
 // whites-platform-api.queue_new_orders
